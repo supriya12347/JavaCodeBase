452d0ed76324dea0f5800b244de4ade1291522e2
supriya12347 <57176649+supriya12347@users.noreply.github.com>
2024-12-13 22:05:31 +0530

    Update MainClass.java

--- App/src/com/mph/demo/MainClass.java
+++ App/src/com/mph/demo/MainClass.java
@@ -2,9 +2,9 @@ package com.mph.demo;
 
 public class MainClass {
 	
-	//Developer1 made changes
+	//Developer11 made changes
 	public static void main(String[] args) {
-		System.out.println("--Developer 1 changes--");
+		System.out.println("--Developer 11 changes--");
 	}
 
 }
